401310d572cbfb5a569485b86d09db55b8161d2d
unknown <abdallah.m.nassar101@gmail.com>
2021-04-11 14:34:55 +0200

    added Erasing functionality

--- painter/lib/app/widgets/custom_app_bar.dart
+++ painter/lib/app/widgets/custom_app_bar.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:painter/app/core/theme/app_theme.dart';
+import 'package:painter/app/modules/home/controllers/home_controller.dart';
 
 // 'with' is like a mixin to allow this class to be of return type
 // [StatelessWidget] and [PreferredSizeWidget]
 class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
   //================================ Properties ================================
+  // I'm getting it like that and not through the constructor as this is
+  // the main point of using 'Get' for dependency injection
+  final homeController = Get.find<HomeController>();
   final String text;
   //================================ Constructor ===============================
-  const CustomAppBar({
+  CustomAppBar({
     required this.text,
   });
   //================================= Methods ==================================
+
+  //============================================================================
   @override
   Widget build(BuildContext context) {
     return AppBar(
@@ -21,6 +28,16 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
       centerTitle: true,
       backgroundColor: AppTheme.appTheme.appBarTheme.backgroundColor,
       actionsIconTheme: AppTheme.appTheme.appBarTheme.actionsIconTheme,
+      actions: [
+        IconButton(
+          icon: Icon(Icons.phonelink_erase),
+          onPressed: () => homeController.setDrawingMode = DrawingMode.PAINT,
+        ),
+        IconButton(
+          icon: Icon(Icons.ac_unit),
+          onPressed: () => homeController.setDrawingMode = DrawingMode.ERASE,
+        ),
+      ],
     );
   }
 
